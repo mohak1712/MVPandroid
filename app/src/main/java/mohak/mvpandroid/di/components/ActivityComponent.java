ebbbf529e8066390d4bbcac66b9f1aa5a684a58b
Mohak <mohakpuri1712@gmail.com>
2017-05-28 19:06:45 +0530

    dagger2 components updated

--- app/src/main/java/mohak/mvpandroid/di/components/ActivityComponent.java
+++ app/src/main/java/mohak/mvpandroid/di/components/ActivityComponent.java
@@ -1,18 +1,28 @@
 package mohak.mvpandroid.di.components;
 
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
 import dagger.Component;
 import mohak.mvpandroid.di.modules.ActivityModule;
+import mohak.mvpandroid.di.modules.NetworkModule;
+import mohak.mvpandroid.ui.Login.LoginActivity;
 import mohak.mvpandroid.ui.Main.MainActivity;
 import mohak.mvpandroid.di.scopes.ActivityScope;
+import mohak.mvpandroid.ui.Main2Activity;
 
 /**
  * Created by mohak on 19/5/17.
  */
 
-@Component(modules = {ActivityModule.class}, dependencies = {ApplicationComponent.class})
 @ActivityScope
+@Component(modules = {ActivityModule.class}, dependencies = {ApplicationComponent.class})
 public interface ActivityComponent {
 
     void injectMainActivity(MainActivity mainActivity);
+
+    void injectLoginActivity(LoginActivity mainActivity);
+
+    void injectMainActivity2(Main2Activity mainActivity);
 
 }
--- app/src/main/java/mohak/mvpandroid/di/components/ApplicationComponent.java
+++ app/src/main/java/mohak/mvpandroid/di/components/ApplicationComponent.java
@@ -2,6 +2,7 @@ package mohak.mvpandroid.di.components;
 
 import dagger.Component;
 import mohak.mvpandroid.MvpAndroid;
+import mohak.mvpandroid.data.DataManager.network.TvDbService;
 import mohak.mvpandroid.di.modules.ApplicationModule;
 import mohak.mvpandroid.di.modules.NetworkModule;
 import mohak.mvpandroid.di.scopes.ApplicationScope;
@@ -10,10 +11,12 @@ import mohak.mvpandroid.di.scopes.ApplicationScope;
  * Created by mohak on 19/5/17.
  */
 
-@Component(modules = {ApplicationModule.class , NetworkModule.class})
 @ApplicationScope
+@Component(modules = {ApplicationModule.class , NetworkModule.class})
 public interface ApplicationComponent {
 
     void inject(MvpAndroid app);
+
+    TvDbService getMovieDb();
 }
 
