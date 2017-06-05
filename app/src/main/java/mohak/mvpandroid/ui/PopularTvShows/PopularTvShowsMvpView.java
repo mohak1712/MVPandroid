e29a92df62b31e92a10fcd541a8c1b5436f50f93
Mohak <mohakpuri1712@gmail.com>
2017-06-05 15:51:31 +0530

    res files updated

--- app/src/main/java/mohak/mvpandroid/ui/PopularTvShows/PopularTvShowsMvpView.java
+++ app/src/main/java/mohak/mvpandroid/ui/PopularTvShows/PopularTvShowsMvpView.java
@@ -7,7 +7,7 @@ import mohak.mvpandroid.ui.Base.BaseMvpView;
  * Created by mohak on 4/6/17.
  */
 
-public interface PopularTvShowsMvp extends BaseMvpView {
+public interface PopularTvShowsMvpView extends BaseMvpView {
 
      void fetchedList(TvModelResult result);
 }
