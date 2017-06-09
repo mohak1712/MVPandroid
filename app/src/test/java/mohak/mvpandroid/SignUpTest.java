c95753952ca724b8ce2f253f82cd5a61c0429996
Mohak <mohakpuri1712@gmail.com>
2017-06-09 17:45:19 +0530

    Unit test wth mockito

--- app/src/test/java/mohak/mvpandroid/SignUpTest.java
+++ app/src/test/java/mohak/mvpandroid/SignUpTest.java
@@ -1,8 +1,56 @@
 package mohak.mvpandroid;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import mohak.mvpandroid.data.DataManager.AppDataManager;
+import mohak.mvpandroid.data.DataManager.DataManager;
+import mohak.mvpandroid.ui.SignUp.SignUpMvpPresenter;
+import mohak.mvpandroid.ui.SignUp.SignUpMvpView;
+import mohak.mvpandroid.ui.SignUp.SignUpPresenter;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 /**
  * Created by mohak on 9/6/17.
  */
 
+@RunWith(MockitoJUnitRunner.class)
 public class SignUpTest {
+
+    String email = "abc1712gmail.com";
+    String pass = "fdgte%$";
+
+    @Mock
+    SignUpMvpView mMockLoginMvpView;
+
+    @Mock
+    DataManager mMockDataManager;
+
+    SignUpMvpPresenter<SignUpMvpView> mvpPresenter;
+
+    @Before
+    public void setUp() throws Exception {
+        mvpPresenter = new SignUpPresenter<>(mMockDataManager);
+        mvpPresenter.onAttach(mMockLoginMvpView);
+    }
+
+    @Test
+    public void signUpTest(){
+
+        when(mMockLoginMvpView.isNetworkAvailable()).thenReturn(true);
+        mvpPresenter.signUpCheck(email,pass);
+        verify(mMockLoginMvpView).showError(R.string.invalid_email_error);
+    }
+
+    @After
+    public void terminate() throws Exception {
+        mvpPresenter.onDetach();
+    }
 }
